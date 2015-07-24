7d3db16082cc7a70f14cfe9a9bccda54b39456d9
Gerardo <oxxido@gmail.com>
2015-07-24 12:39:30 +0000

    | INITIAL COMMIT | | Project: Certified Comments | Git repo: local | Source: git bare project

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,50 @@
+<?php namespace App\Http\Controllers;
+
+class WelcomeController extends Controller {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Welcome Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller renders the "marketing page" for the application and
+	| is configured to only allow guests. Like most of the other sample
+	| controllers, you are free to modify or remove it as you desire.
+	|
+	*/
+
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware('guest');
+	}
+
+	/**
+	 * Show the application welcome screen to the user.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//return view('welcome');
+		$data = [];
+		$data['laravelEnabled'] = "Laravel Ok";
+		return view('home-ng', $data);
+	}
+	/**
+	 * Second template demo
+	 *
+	 * @return Response
+	 */
+	public function creative()
+	{
+		//return view('welcome');
+		$data = [];
+		$data['laravelEnabled'] = "Laravel Ok";
+		return view('themes/creative/home', $data);
+	}
+}
---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,9 @@
+      <ul class="footer_menu">
+
+      </ul>
+  <!-- scripts  -->
+ 
+  <script type="text/javascript" src="{{ asset('vendor/angular/angular.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('vendor/jquery/dist/jquery.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
