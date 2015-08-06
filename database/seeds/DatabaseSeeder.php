a258d1f4adc56b156f15daa53ba0f6b8008087ff
Gerardo <oxxido@gmail.com>
2015-08-06 08:34:24 -0400

    starting to work on seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,33 @@ class DatabaseSeeder extends Seeder {
 	{
 		Model::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('OrganizationTableSeeder');
+		$this->command->info("Organization table seeded");
+		$this->call('typeTableSeeder');
+		$this->command->info("Business Type table seeded");
 	}
 
+
+}
+
+class UsersTableSeeder extends Seeder {
+
+	public function OrganizationTableSeeder() {
+
+		//delete users table records
+         DB::table('organizations')->delete();
+         //insert records
+         DB::table('organizations')->insert(array(
+         	array('name' => 'Company'),
+         	array('name' => 'Corporation'),
+         	array('name' => 'Non profit'),
+         	array('name' => 'School'),
+         	array('name' => 'Office'),
+         	array('name' => 'Agency'),
+         	array('name' => 'Church'),
+         	array('name' => 'Restaurant'),
+         	array('name' => 'Firm'),
+         	array('name' => 'Actual Company Name')
+         	));
+	}
 }
