d1c815d73219d3c974eefdb1bbef92eb0bdac449
Gerardo <oxxido@gmail.com>
2015-08-24 15:52:27 -0400

    https://app.asana.com/0/45931566301109/46551110412341 finish seed and iconset

--- database/seeds/SocialNetworksTableSeeder.php
+++ database/seeds/SocialNetworksTableSeeder.php
@@ -16,8 +16,20 @@ class SocialNetworksTableSeeder extends Seeder
 
         //insert records
         DB::table('social_networks')->insert([
-            ['name' => 'Facebook', 'logo' => '/facebook.jpg', 'url' => 'www.facebook.com'],
-            ['name' => 'Twitter', 'logo' => '/twitter.jpg', 'url' => 'www.twitter.com']
+            ['name' => 'Facebook',      'logo' => '/images/circular_icon_set/facebook.png',     'url' => 'facebook.com/%'],
+            ['name' => 'Twitter',       'logo' => '/images/circular_icon_set/twitter.png',      'url' => 'twitter.com/%'],
+            ['name' => 'Blogger',       'logo' => '/images/circular_icon_set/blogger.png',      'url' => '%.blogspot.com'],
+            ['name' => 'Delicious',     'logo' => '/images/circular_icon_set/delicious.png',    'url' => 'delicious.com/%'],
+            ['name' => 'Digg',          'logo' => '/images/circular_icon_set/digg.png',         'url' => 'digg.com/%'],
+            ['name' => 'eBay',          'logo' => '/images/circular_icon_set/ebay.png',         'url' => 'stores.ebay.com/%'],
+            ['name' => 'Flickr',        'logo' => '/images/circular_icon_set/flickr.png',       'url' => 'flickr.com/photos/%'],
+            ['name' => 'Google plus',   'logo' => '/images/circular_icon_set/google.png',       'url' => 'google.com/+%'],
+            ['name' => 'Linkedin',      'logo' => '/images/circular_icon_set/linkedin.png',     'url' => 'linkedin.com/%'],
+            ['name' => 'My Space',      'logo' => '/images/circular_icon_set/myspace.png',      'url' => 'myspace.com/%'],
+            ['name' => 'reddit',        'logo' => '/images/circular_icon_set/reddit.png',       'url' => 'www.reddit.com/r/%'],
+            ['name' => 'Stumbleupon',   'logo' => '/images/circular_icon_set/stumbleupon.png',  'url' => 'www.stumbleupon.com/stumbler/%'],
+            ['name' => 'Wordpress',     'logo' => '/images/circular_icon_set/wordpress.png',    'url' => '%.wordpress.com'],
+            ['name' => 'Youtube',       'logo' => '/images/circular_icon_set/youtube.png',      'url' => 'youtube.com/c/%']
          ]);
     }
 }
