6141fbfebe603fed425eedf3051f3820c15f6f14
Juan Pablo Pepe <juanpapepe@gmail.com>
2015-09-02 10:40:41 -0300

    migration name changed

--- database/migrations/2015_09_02_133658_alter_businesses_table_add_data_column.php
+++ database/migrations/2015_09_02_133658_alter_businesses_table_add_data_column.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterBusinessesTable extends Migration
+class AlterBusinessesTableAddDataColumn extends Migration
 {
     /**
      * Run the migrations.
