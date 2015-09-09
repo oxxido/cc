fbc2d3b3ae655eb8d63cba6b8686edbb9c683f10
Juan Pablo Pepe <juanpapepe@gmail.com>
2015-09-09 11:05:49 -0300

    fixed reset password

--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -1,59 +1,43 @@
-@extends('app')
+@extends('auth.layout')
 
 @section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Reset Password</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-						<input type="hidden" name="token" value="{{ $token }}">
+<h2>Reset Password</h2>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">Confirm Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password_confirmation">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									Reset Password
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
+@if (count($errors) > 0)
+	<div class="alert alert-danger">
+		<strong>Whoops!</strong> There were some problems with your input.<br><br>
+		<ul>
+			@foreach ($errors->all() as $error)
+				<li>{{ $error }}</li>
+			@endforeach
+		</ul>
 	</div>
-</div>
+@endif
+
+<div style="height:20px;"></div>
+
+<form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
+	<input type="hidden" name="_token" value="{{ csrf_token() }}">
+	<input type="hidden" name="token" value="{{ $token }}">
+
+    <input type="email" class="loginformfield" value="{{ old('email') }}" placeholder="Your Email" id="email"  name="email" required > 
+	<input type="password" class="loginformfield" value="" placeholder="Your Password" id="password"  name="password" required>
+	<input type="password" class="loginformfield" value="" placeholder="Confirm Password"   name="password_confirmation" required>
+
+    <input type="image" src="/images/btn_send.png" id="imageField" name="submit">
+</form>
+
+@endsection
+
+@section('footer')
+  <script type="text/javascript" src="{{ asset('/vendor/pwstrength-bootstrap/dist/pwstrength-bootstrap-1.2.7.min.js') }}"></script>
+  <script type="text/javascript">
+    $(function () {
+      $('#password').pwstrength({ui: {
+          showVerdictsInsideProgressBar: true
+          
+        } });
+    });
+  </script>
 @endsection
