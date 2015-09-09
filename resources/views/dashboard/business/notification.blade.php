719ec9a6ce6b649262910ea2a8dae963683e82a4
Gerardo <oxxido@gmail.com>
2015-09-09 09:25:14 -0400

    Create Notifications UI https://app.asana.com/0/48627073471099/48952901013112

--- resources/views/dashboard/business/notification.blade.php
+++ resources/views/dashboard/business/notification.blade.php
@@ -27,7 +27,42 @@
       </div>
       <div class="panel panel-default">
         <div class="panel-body">
-          <pre>{{ print_r($config,true) }}</pre>
+        <p>This section allows you to choose which feedback events you want to receive email 
+        alerts for and who should receive them.</p>
+        <h4>Send to</h4>
+          <div class="form-group">
+            <input type="checkbox" name="include_social_links" id="include_social_links" value="1"  >
+            <label for="include_social_links" >Account Owner</label>
+          </div>
+          <div class="form-group">
+            <input type="checkbox" name="include_social_links" id="include_social_links" value="1" >
+            <label for="include_social_links" >Business Manager</label>
+          </div>
+          <br />
+          <h4>Which alerts?</h4>
+          <div class="form-group row">
+            <div class="col-sm-3"> 
+              <select class="form-control " >
+               <option>Positive Feedback</option>
+               <option>Negative Feedback</option>
+               <option>Both Positive and Negative</option>
+              </select>
+            </div>
+          </div>
+          <br />
+          <h4>What to send?</h4>
+          <div class="form-group">
+            <div >
+            <input type="checkbox" name="include_social_links" id="include_social_links" value="1"  >
+            <label for="include_social_links" >Send new online review alerts</label>
+          </div>
+          <div class="checkbox">
+          <label for="performance_report" >
+            <input type="checkbox" name="performance_report" id="performance_report" value="1" > <b>Send performance report</b>
+          </label>
+          </div>
+          </div>
+
         </div>
         <div class="box-footer">
           <button class="btn btn-primary" type="submit">Save</button>
