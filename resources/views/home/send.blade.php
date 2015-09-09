49822e2b0711866adfb41462201c5179c9c20b3a
Juan Pablo Pepe <juanpapepe@gmail.com>
2015-09-09 13:20:50 -0300

    change message on send invite page

--- resources/views/home/send.blade.php
+++ resources/views/home/send.blade.php
@@ -3,6 +3,6 @@
 @section('content')
   <img width="256" vspace="20" height="256" src="/images/inviteicon.png">
   <h2>Thanks for your interest!</h2>
-  Leave us your information and we'll send you an invite as soon as we're ready.
+  We'll send you an invite as soon as we're ready.
   <div style="height:20px;"></div>
 @endsection
