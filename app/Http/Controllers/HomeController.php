0e388e5c4075d997eb132b0bac826c110a492aae
Juan José Coello Cabrera <automaticko@gmail.com>
2015-09-10 10:13:44 -0300

    Modifies codesstyle on HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,50 +2,41 @@
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller {
-
-  /**
-   * show the invite request to the user
-   *
-   * @return Response
-   */
-  public function send(Request $request)
-  {
-    $data = array(
-        'name'    => $request->input('name'),
-        'company' => $request->input('company'),
-        'email'   => $request->input('email'),
-        'website' => $request->input('website')
-    );
-
-    // Instantiate validator using received post parameters and setted rules
-    $validation = \Validator::make(\Request::all(), [
-        'name'    => 'required',
-        'company' => 'required',
-        'email'   => "required|email",
-        'website'     => 'required|url'
-    ]);
-
-    if ($validation->fails())
+class HomeController extends Controller
+{
+    /**
+     * show the invite request to the user
+     *
+     * @return Response
+     */
+    public function send(Request $request)
     {
-        return redirect()
-            ->back()
-            ->withInput()
-            ->withErrors($validation->getMessageBag()->toArray());
-    }
-    else
-    {
-        if($request->input('source')=="contact")
-        {
-            $data['msg'] = $request->input('msg');
-            $this->email()->contact($data);
-        }
-        else
-        {
-            $this->email()->invite($data);
-        }
-    }
+        $data = [
+            'name'    => $request->input('name'),
+            'company' => $request->input('company'),
+            'email'   => $request->input('email'),
+            'website' => $request->input('website')
+        ];
 
-    return view('home.send');
-  }
+        // Instantiate validator using received post parameters and setted rules
+        $validation = \Validator::make(\Request::all(), [
+            'name'    => 'required',
+            'company' => 'required',
+            'email'   => "required|email",
+            'website' => 'required|url'
+        ]);
+
+        if ($validation->fails()) {
+            return redirect()->back()->withInput()->withErrors($validation->getMessageBag()->toArray());
+        } else {
+            if ($request->input('source') == "contact") {
+                $data['msg'] = $request->input('msg');
+                $this->email()->contact($data);
+            } else {
+                $this->email()->invite($data);
+            }
+        }
+
+        return view('home.send');
+    }
 }
