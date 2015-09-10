947185839a29eb54a9ca0227f7bdb25b082ba1aa
Juan José Coello Cabrera <automaticko@gmail.com>
2015-09-10 10:12:22 -0300

    Creates template mail for thanks after invite (copy paste)

---
+++ resources/views/emails/thanks.blade.php
@@ -0,0 +1,22 @@
+@extends('emails.layout')
+
+@section('body')
+  <table class="row">
+    <tr>
+      <td class="wrapper last">
+        <table class="twelve columns">
+          <tr>
+            <td>
+              <h1>Requested Invite</h1>
+              <p class="lead">Thank you for join us</p>
+              <p>
+                Soon you will receive indications on how to proceed.
+              </p>
+            </td>
+            <td class="expander"></td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>
+@endsection
