38905a74ab7f9863e9974754741909310ddf6b67
Juan José Coello Cabrera <automaticko@gmail.com>
2015-09-11 09:31:22 -0300

    Modifies EventServiceProvider to fire created comment event

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -1,20 +1,24 @@
 <?php namespace App\Providers;
 
-use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
+use App\Events\Comment\Created;
+use App\Listeners\Comment\SendEmails;
+use App\Models\Comment;
+use Event;
 
-class EventServiceProvider extends ServiceProvider {
-
-	/**
-	 * The event handler mappings for the application.
-	 *
-	 * @var array
-	 */
-	protected $listen = [
-		'event.name' => [
-			'EventListener',
-		]
-	];
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event handler mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        Created::class => [
+            SendEmails::class
+        ]
+    ];
 
     /**
      * The subscriber classes to register.
@@ -25,17 +29,19 @@ class EventServiceProvider extends ServiceProvider {
         'App\Listeners\UserEmailEventListener',
     ];
 
-	/**
-	 * Register any other events for your application.
-	 *
-	 * @param  \Illuminate\Contracts\Events\Dispatcher  $events
-	 * @return void
-	 */
-	public function boot(DispatcherContract $events)
-	{
-		parent::boot($events);
+    /**
+     * Register any other events for your application.
+     *
+     * @param  \Illuminate\Contracts\Events\Dispatcher $events
+     *
+     * @return void
+     */
+    public function boot(DispatcherContract $events)
+    {
+        parent::boot($events);
 
-		//
-	}
-
+        Comment::created(function (Comment $comment) {
+            Event::fire(Created::class, [$comment]);
+        });
+    }
 }
