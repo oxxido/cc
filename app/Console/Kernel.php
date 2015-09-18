056a257b85b7401481f14c095e8055b62b964866
Juan José Coello Cabrera <automaticko@gmail.com>
2015-09-18 13:51:02 -0300

    Creates method on scheduler to automatically send feedback requests

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -6,6 +6,7 @@ use App\Console\Commands\DropDatabase;
 use App\Console\Commands\Inspire;
 use App\Services\ReportService;
 use App\Services\EmailService;
+use App\Models\Commenter;
 use App\Models\Business;
 use DB;
 
@@ -42,18 +43,34 @@ class Kernel extends ConsoleKernel
         $schedule->call(function () {
             $this->reportBusinessesByFrequency(Business::CONFIG_NOTIFICATIONS_FREQUENCY_MONTHLY);
         })->monthly();
+        $schedule->call(function () {
+            $this->automaticFeedbackRequest();
+        })->weekly();
     }
 
     protected function reportBusinessesByFrequency($frequency)
     {
-        $json_frequency = '"frequency":' . $frequency;
+        $json_frequency    = '"frequency":' . $frequency;
         $json_send_reports = '"send_reports":true';
-        $businesses = Business::where(DB::raw("INSTR(data, '{$json_frequency}')"), '<>', 0)->where(
-            DB::raw("INSTR(data, '{$json_send_reports}')"), '<>', 0
-        )->get();
+        $businesses        = Business::where(DB::raw("INSTR(data, '{$json_frequency}')"), '<>',
+            0)->where(DB::raw("INSTR(data, '{$json_send_reports}')"), '<>', 0)->get();
 
         foreach ($businesses as $business) {
             EmailService::instance()->performanceReport(ReportService::basicPerformanceReport($business->id));
+        }
+    }
+
+    protected function automaticFeedbackRequest()
+    {
+        $commenters = Commenter::with('user',
+            'businessCommenters.business.products')->select(['commenters.*'])->join('business_commenter AS bc',
+            function ($join) {
+                $join->on('commenters.id', '=', 'bc.commenter_id');
+                $join->on('bc.request_feedback_automatically', '=', DB::raw(true));
+            })->groupBy('commenters.id')->get();
+
+        foreach ($commenters as $commenter) {
+            EmailService::instance()->requestFeedback($commenter);
         }
     }
 }
