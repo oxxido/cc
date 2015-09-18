7e2dd637c423019904cfc476cb2a757cecd93bef
Juan José Coello Cabrera <automaticko@gmail.com>
2015-09-18 09:21:47 -0300

    Creates lang files for user and commenter

---
+++ resources/lang/en/user.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'fields' => [
+        'first_name'            => 'First Name',
+        'last_name'             => 'Last Name',
+        'email'                 => 'E-mail',
+        'current_password'      => 'Current Password',
+        'password'              => 'Password',
+        'password_confirmation' => 'Repeat Password',
+    ]
+];
