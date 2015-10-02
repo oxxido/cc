a1a502ce021a4ed7b69976f3b20e1a89d0829abf
Alfredo Cuellar <alfrecuellar@hotmail.com>
2015-10-02 10:53:58 -0300

    Pusher log

--- public/js/cc.pusher.js
+++ public/js/cc.pusher.js
@@ -4,9 +4,6 @@ cc.pusher = {
         this.instance = new Pusher(cc.pusher_key, {
             encrypted: true
         });
-        this.instance.connection.bind('state_change', function(states) {
-          console.log(states);
-        });
     },
     subscribe : function(name, event, callback){
         var channel = this.instance.subscribe(name);
