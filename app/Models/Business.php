2b73333b412508726e5a8e0520e4bbe7e0d962c7
Juan José Coello Cabrera <automaticko@gmail.com>
2015-10-02 09:01:13 -0300

    Modifies Business model * Adds id and request_feedback_sent columns to pivot on   relation method to commenter

--- app/Models/Business.php
+++ app/Models/Business.php
@@ -137,7 +137,7 @@ class Business extends Model
 
     public function commenters()
     {
-        return $this->belongsToMany(Commenter::class)->withPivot('adder_id', 'request_feedback_automatically');
+        return $this->belongsToMany(Commenter::class)->withPivot('id', 'adder_id', 'request_feedback_automatically', 'feedback_requests_sent');
     }
 
     /**
