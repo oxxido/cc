39c8d11ce5cb816c1e3f33bf115e525c97f14c77
Alfredo Cuellar <alfrecuellar@hotmail.com>
2015-10-08 11:37:23 -0300

    Add dashboard link on home

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -20,6 +20,7 @@ Free 15 Day Trial (Button)
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ str_limit(Auth::user()->name,10) }} <span class="caret"></span></a>
                 <ul class="dropdown-menu" role="menu">
+                    <li><a href="{{ url('dashboard') }}">Dashboard</a></li>
                     <li><a href="{{ url('auth/logout') }}">Logout</a></li>
                 </ul>
             </li>
