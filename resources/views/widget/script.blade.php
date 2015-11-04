8af20714e9356245875300dfddf0e6c212df779c
Alfredo Cuellar <alfrecuellar@hotmail.com>
2015-11-04 15:16:23 -0300

    URL fixed

--- resources/views/widget/script.blade.php
+++ resources/views/widget/script.blade.php
@@ -4,7 +4,7 @@ window.addEventListener("load", function(){
     div.style.height="500px";
     div.style.border="1px solid #ddd";
     var iframe = document.createElement("IFRAME");
-    iframe.src="{{ url("http://cc.xeedlabs.localhost/widget/{$type}/iframe/{$product_uuid}") }}";
+    iframe.src="{{ url("widget/{$type}/iframe/{$product_uuid}") }}";
     iframe.style.border="0px";
     iframe.style.width="100%";
     iframe.style.height="100%";
